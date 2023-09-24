4fe1c0627e611be520f460a4c9e1ec05e174d6e5
alvadeli <142695804+alvadeli@users.noreply.github.com>
2023-09-24 20:18:25 +0200

    Implemented ContactList

---
+++ app/src/main/java/com/example/android_contactlist/ContactDao.kt
@@ -0,0 +1,28 @@
+package com.example.android_contactlist
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ContactDao {
+
+    //suspend -> run in coroutine -> block until database operation is finished
+    @Upsert
+    suspend fun upsertContact(contact: Contact) : Long
+
+    @Delete
+    suspend fun deleteContact(contact: Contact)
+
+    @Query("SELECT * FROM contact ORDER BY firstName ASC")
+    fun getContactsOrderedByFirstName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact ORDER BY lastName ASC")
+    fun getContactsOrderedByLastName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact ORDER BY phoneNumber ASC")
+    fun getContactsOrderedByphoneNumber(): Flow<List<Contact>>
+
+}
